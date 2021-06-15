20aed02453d863825ef8ad409da21b6c40e25f56
jaredleys <jaredleys@gmail.com>
2021-06-14 22:56:59 -0400

    update html

--- mars_app.py
+++ mars_app.py
@@ -7,7 +7,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def home():
-    mars_data = mongo.db.mars_info.find_one()
+    mars_data = mongo.db.collection.find_one()
     return render_template("index.html", mars=mars_data)
 
 @app.route("/scrape")
